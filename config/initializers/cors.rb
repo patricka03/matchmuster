3abd8a236e922b007c8a3d0e33dfef60c8ddaa25
Patrick Amfo <patrick.amfo@icloud.com>
2026-08-13 15:50:56 +0100

    Allow MatchMuster production frontend in CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173"
+    origins(
+      "http://localhost:5173",
+      "https://matchmuster.uk",
+      "https://www.matchmuster.uk"
+    )
 
     resource "*",
       headers: :any,
